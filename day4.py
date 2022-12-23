c81f8e19efbf0d50110b56dcf7f431162b2050a9
EthanDenny <ethandenny@protonmail.com>
2022-12-22 21:24:23 -0330

    Day 4
    Best attempt. Doesn't work for all cases.

---
+++ day4.py
@@ -0,0 +1,29 @@
+def f(nums):
+    i = 0
+    while i < len(nums):
+        n = nums[i]
+
+        if 0 < n < len(nums):
+            temp = nums[n]
+            nums[n] = nums[i]
+            nums[i] = temp
+            if n == temp:
+                i += 1
+        else:
+            i += 1
+
+    low = 1
+    for n in nums:
+        if low == n:
+            low += 1
+    return low
+
+
+if __name__ == "__main__":
+    nums = [3, 4, -1, 1]
+    result = f(nums)
+    print(result)
+
+    nums = [2, 1, 3, 6]
+    result = f(nums)
+    print(result)
